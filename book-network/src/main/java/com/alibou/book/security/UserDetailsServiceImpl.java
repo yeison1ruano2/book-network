db431db634cd4d8d7eb7d6ca4a617a880b05dc0e
yeison1ruano2 <yeisonstiven100@gmail.com>
2024-08-06 20:41:25 -0500

    Primer commit de inicialización

---
+++ book-network/src/main/java/com/alibou/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.alibou.book.security;
+
+
+import com.alibou.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository repository;
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+    return repository.findByEmail(userEmail).orElseThrow(()-> new UsernameNotFoundException("Usuario no encontrado"));
+  }
+}
